539c96a3ce2be5a5162c62f3fa69b250a7c4519b
Satu Koskinen <satu.a.koskinen@gmail.com>
2021-02-28 20:54:36 +0200

    first ready version of ft_modf

--- ft_modf.c
+++ ft_modf.c
@@ -6,12 +6,11 @@
 /*   By: skoskine <skoskine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 08:42:46 by skoskine          #+#    #+#             */
-/*   Updated: 2021/02/16 08:43:44 by skoskine         ###   ########.fr       */
+/*   Updated: 2021/02/28 19:21:16 by skoskine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <inttypes.h>
-//#include "libft.h"
+#include "libft.h"
 
 double		ft_modf(double value, double *iptr)
 {
@@ -20,24 +19,22 @@ double		ft_modf(double value, double *iptr)
 	short				exponent;
 	unsigned long long	significand;
 
+	*iptr = value;
+	if (value == 0.0 || value == -0.0 || ft_isnan(value))
+		return (value);
+	else if (ft_isposinf(value) || ft_isneginf(value))
+		return (ft_isposinf(value) ? 0.0 : -0.0);
 	double_as_int = (unsigned long long*)&value;
-//	printf("double as int %llx\n", *double_as_int);
 	sign = (unsigned char)(*double_as_int >> 63);
 	exponent = (unsigned short)(*double_as_int >> 52 & 0x7FF) - 1023;
-	significand = (*double_as_int & 0x000FFFFFFFFFFFFFFULL) | (1ULL << 52);
-//	printf("sign %hhx\nexponent %hd\nsignificand %llx\n", sign, exponent, significand);
-//	printf("sign %s \nexponent %s\nsignificand %s\n", ft_uintmax_itoa_base((uintmax_t)sign, 2, 0), ft_uintmax_itoa_base((uintmax_t)exponent, 2, 0), ft_uintmax_itoa_base((uintmax_t)significand, 2, 0));
-//	printf("significand %llu\nshifted%llu\n", significand, significand >> (52-exponent));
+	significand = (*double_as_int & 0x0000FFFFFFFFFFFFFULL) | (1ULL << 52);
 	if (exponent < 0)
-		*iptr = 0;
-	else if (exponent >= 52)
-		*iptr = value;
+		*iptr = (sign == 0 ? 0.0 : -0.0);
 	else if (exponent == 0)
 		*iptr = (sign == 0 ? 1.0 : -1.0);
+	else if (exponent > 52)
+		*iptr = value;
 	else
 		*iptr = (sign == 0 ? 1.0 : -1.0) * (significand >> (52 - exponent));
-	if (sign == 0)
-		return (value - *iptr);
-	else
-		return (value + *iptr);
+	return (value - *iptr);
 }

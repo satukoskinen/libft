a88ad2668fd69633651c4948b1c9d92b0a260672
Satu Koskinen <satu.a.koskinen@gmail.com>
2021-03-14 17:27:23 +0200

    some fixes

--- ft_isposinf.c
+++ ft_isposinf.c
@@ -6,11 +6,11 @@
 /*   By: skoskine <skoskine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/15 22:36:22 by skoskine          #+#    #+#             */
-/*   Updated: 2021/02/18 18:45:32 by skoskine         ###   ########.fr       */
+/*   Updated: 2021/03/14 17:24:05 by skoskine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_isposinf(double nbr)
 {
-	return (nbr == 1.0 / 0 ? 1 : 0);
+	return (nbr == 1.0 / 0.0 ? 1 : 0);
 }

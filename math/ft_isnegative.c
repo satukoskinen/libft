495918e6fec27e38388d175a45fa80f64b617353
Satu Koskinen <satu.a.koskinen@gmail.com>
2021-03-31 09:16:42 +0300

    removed strict-aliasing violating code

--- math/ft_isnegative.c
+++ math/ft_isnegative.c
@@ -6,16 +6,18 @@
 /*   By: skoskine <skoskine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 08:40:49 by skoskine          #+#    #+#             */
-/*   Updated: 2021/02/18 18:45:51 by skoskine         ###   ########.fr       */
+/*   Updated: 2021/03/31 08:43:34 by skoskine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 int		ft_isnegative(double nbr)
 {
-	unsigned long long	*double_as_int;
+	unsigned long long	double_as_int;
 	unsigned char		sign;
 
-	double_as_int = (unsigned long long*)&nbr;
-	sign = (unsigned char)(*double_as_int >> 63);
+	ft_memcpy(&double_as_int, &nbr, sizeof(nbr));
+	sign = (unsigned char)(double_as_int >> 63);
 	return ((sign == 0) ? 0 : 1);
 }
--- math/ft_modf.c
+++ math/ft_modf.c
@@ -6,7 +6,7 @@
 /*   By: skoskine <skoskine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 08:42:46 by skoskine          #+#    #+#             */
-/*   Updated: 2021/02/28 19:21:16 by skoskine         ###   ########.fr       */
+/*   Updated: 2021/03/31 08:43:22 by skoskine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 double		ft_modf(double value, double *iptr)
 {
-	unsigned long long	*double_as_int;
+	unsigned long long	double_as_int;
 	unsigned char		sign;
 	short				exponent;
 	unsigned long long	significand;
@@ -24,10 +24,10 @@ double		ft_modf(double value, double *iptr)
 		return (value);
 	else if (ft_isposinf(value) || ft_isneginf(value))
 		return (ft_isposinf(value) ? 0.0 : -0.0);
-	double_as_int = (unsigned long long*)&value;
-	sign = (unsigned char)(*double_as_int >> 63);
-	exponent = (unsigned short)(*double_as_int >> 52 & 0x7FF) - 1023;
-	significand = (*double_as_int & 0x0000FFFFFFFFFFFFFULL) | (1ULL << 52);
+	ft_memcpy(&double_as_int, &value, sizeof(value));
+	sign = (unsigned char)(double_as_int >> 63);
+	exponent = (unsigned short)(double_as_int >> 52 & 0x7FF) - 1023;
+	significand = (double_as_int & 0x0000FFFFFFFFFFFFFULL) | (1ULL << 52);
 	if (exponent < 0)
 		*iptr = (sign == 0 ? 0.0 : -0.0);
 	else if (exponent == 0)
